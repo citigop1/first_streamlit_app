8561c890c6dec36cd8a1770a04f46c5d46da69e4
citigop1 <126320170+citigop1@users.noreply.github.com>
2023-02-25 19:17:25 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,11 @@ streamlit.text('🥑 Avocado Toast🍞');
 streamlit.header('🍊🫐Build your Own Fruit Smoothie🍑🍓');
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index))
+
+#streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index))
+
 fruits_selected = streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index), ['Avocado','Strawberries']);
+
 fruits_to_show=my_fruit_list.loc(fruits_selected)
 streamlit.dataframe(fruits_to_show)
 
