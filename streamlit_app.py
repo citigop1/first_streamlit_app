e3331781e2fb7d842f583a6068b1498500c0a947
citigop1 <126320170+citigop1@users.noreply.github.com>
2023-02-25 09:11:40 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,4 @@ streamlit.text('Omega 3 & Blueberry Oatmeal');
 
 streamlit.text('Kale, Spinach & Rocket Smoothie');
 
-strealit.text('Hard-Boiled Free-Range Egg');
+streamlit.text('Hard-Boiled Free-Range Egg');
