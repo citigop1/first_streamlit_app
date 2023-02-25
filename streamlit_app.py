c25fb0be41695eab3ae952b4d165d40f5e696fe7
citigop1 <126320170+citigop1@users.noreply.github.com>
2023-02-25 09:21:14 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ import streamlit
 streamlit.title ('My Parents New Healthy Diner');
 
 streamlit.header('Breakfast menu');
-    streamlit.text('🫐Omega 3 & Blueberry Oatmeal');
-    streamlit.text('🥬 Kale, Spinach & Rocket Smoothie');
-    streamlit.text('🍳Hard-Boiled Free-Range Egg');
-    streamlit.text('🥑 Avocado Toast🍞');
+streamlit.text('🫐Omega 3 & Blueberry Oatmeal');
+streamlit.text('🥬 Kale, Spinach & Rocket Smoothie');
+streamlit.text('🍳Hard-Boiled Free-Range Egg');
+streamlit.text('🥑 Avocado Toast🍞');
 
 
 streamlit.header('Build your Own Fruit Smoothie');
