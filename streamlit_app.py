f5ef7a1b9ebe522128591e3af7b789da01119350
citigop1 <126320170+citigop1@users.noreply.github.com>
2023-02-25 09:26:02 +0000

    Update streamlit_app.py
    Fixed Typo read_csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ streamlit.text('🥑 Avocado Toast🍞');
 
 
 streamlit.header('🍊🫐Build your Own Fruit Smoothie🍑🍓');
-my_fruit_list=pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 streamlit.dataframe(my_fruit_list);
 
