e835b85643c5c8dcc456dda9c71ae2aaa913660a
citigop1 <126320170+citigop1@users.noreply.github.com>
2023-03-01 22:03:51 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,5 +59,6 @@ add_my_fruit=streamlit.text_input('What fruit would like to add?')
 streamlit.write('The user entered', add_my_fruit)
  
 streamlit.text("Thanks for adding " +add_my_fruit)
+mycur.execute("insert into fruit_load_list values('From Streamlit')";
 
 
